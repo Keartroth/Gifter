f31c0e79f8437a4730279feb286bb782ac55e031
Keartroth <mecarrolljr@gmail.com>
2020-06-30 15:47:48 -0500

    initial commit

---
+++ Gifter/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public Post Post { get; set; }
+    }
+}
